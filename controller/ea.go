cd48c057fdae799c9ec793692a7cd9ecb1e776af
Ashrockzzz2003 <ashrockzzz2003@gmail.com>
2025-02-27 16:39:03 +0530

    Basic version of code generator for EA done.

--- controller/ea.go
+++ controller/ea.go
@@ -1,19 +1,39 @@
 package controller
 
 import (
+	"evolve/modules"
 	"evolve/util"
 	"net/http"
+	"os"
 )
 
 func CreateEA(res http.ResponseWriter, req *http.Request) {
 	var logger = util.NewLogger()
 	logger.Info("CreateEA API called.")
 
+	// TODO: Add Auth.
+
 	data, err := util.Body(req)
 	if err != nil {
 		util.JSONResponse(res, http.StatusBadRequest, err.Error(), nil)
 		return
 	}
+
+	ea, err := modules.EAFromJSON(data)
+	if err != nil {
+		util.JSONResponse(res, http.StatusBadRequest, err.Error(), nil)
+		return
+	}
+
+	code, err := ea.Code()
+	if err != nil {
+		util.JSONResponse(res, http.StatusBadRequest, err.Error(), nil)
+		return
+	}
+
+	// TODO: Write code for MinIO and remove this.
+	os.Mkdir("code", 0755)
+	os.WriteFile("code/ea.py", []byte(code), 0644)
 
 	util.JSONResponse(res, http.StatusOK, "It works! 👍🏻", data)
 }
